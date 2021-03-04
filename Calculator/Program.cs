084ecea4ae3cd705e0b90ab2553abf4b424801ed
danuiii <danu@danujanssen.nl>
2021-03-04 18:49:25 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num01;
+            int num02;
+
+            Console.WriteLine("Voer een nummer in zonder komma.");
+            num01 = Convert.ToInt32( Console.ReadLine() );
+
+            Console.WriteLine("Voer nu nog een nummer in.");
+            num02 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num01 * num02;
+            Console.WriteLine("Het antwoord is " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
